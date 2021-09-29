d75c4a24d5ddb801de9adc357b4b9bd6ffed6268
Andry Saprigin <coffeei.2002@gmail.com>
2021-09-29 11:52:17 +0300

    FourtenthTask

---
+++ №14/Records/auto_record.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Records
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Tom", 36);
+            Console.WriteLine(person.Name); // Tom
+
+            var (userName, userAge) = person;
+
+            Console.WriteLine(userAge);     // 36
+            Console.WriteLine(userName);    // Tom
+        }
+    }
+    public record Person(string Name, int Age);
+    //  Для типа Person будет создаваться конструктор, который принимает два параметра и присваивает их значения соответственно свойствам Name и Age, и что также автоматически будет создаваться деконструктор.
+}
---
+++ №14/Records/record1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+/*
+ * Records представляют новый ссылочный тип, который появился в C#9. Ключевая особенность records состоит в том, что они могут представлять неизменяемый (immutable) тип, который по умолчанию обладает рядом дополнительных возможностей по сравнению с классами. Зачем нам нужны неизменяемые типы? Такие типы более безопасны в тех ситуациях, когда нам надо гарантировать, что данные объекта не будут изменяться. 
+ 
+ */
+namespace Records
+{
+    public record Point
+    {
+        public int X { get; init; }
+        public int Y { get; init; }
+
+        public Point() { }
+
+        public Point(int x, int y)
+        {
+            X = x; Y = y;
+        }
+        // реализован деконструктор, который позволяет разложить объект Person на кортеж значений
+        public void Deconstruct(out int x, out int y) => (x, y) = (X, Y);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point(1, 3);
+
+            Point point2 = point with { X = 10 };
+
+            var (x, y) = point2;
+
+            int X = x;
+            int Y = y;
+        }
+    }
+  
+}
