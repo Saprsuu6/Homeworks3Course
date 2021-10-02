80f3a9cd41954265dd0ee0c311c9d98ff6ee6620
Andry Saprigin <coffeei.2002@gmail.com>
2021-10-02 14:10:54 +0300

    SixtenthTask

---
+++ №16/ReadCs/Changing.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadCs
+{
+    class Changing
+    {
+        string line;
+        string prevLine = null;
+        int countTabs = 0;
+        private string[] array = null;
+
+        public void Split() => array = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        public void UpperCase()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length > 2)
+                {
+                    array[i] = array[i].ToUpper();
+                }
+            }
+        }
+        public void Join() => line = string.Join(" ", array);
+        public void Tabulation()
+        {
+            if (prevLine != null &&
+                (countTabs > 0 || prevLine.Contains('{') || prevLine.Contains('}')))
+            {
+                if (prevLine.Contains('}'))
+                    countTabs--;
+
+                if (!prevLine.Contains('{'))
+                    countTabs--;
+
+                for (int i = 0; i < countTabs + 1; i++)
+                {
+                    line = line.Insert(0, "\t");
+                }
+
+                countTabs++;
+            }
+        }
+
+        public string Line { get { return line; }  set { line = value; } }
+        public string PrevLine { get { return prevLine; } set { prevLine = value; } }
+        public int CountTabs { get { return countTabs; } set { countTabs = value; } }
+    }
+}
---
+++ №16/ReadCs/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReadCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StringWriter newBufer = new StringWriter())
+                {
+                    string path = "Program.cs";
+                    using (StreamReader reader = new StreamReader(path))
+                    {
+                        Changing change = new Changing();
+                        StringBuilder bufer = new StringBuilder();
+                        while ((change.Line = reader.ReadLine()) != null)
+                        {
+                            if (!string.IsNullOrWhiteSpace(change.Line))
+                            {
+                                // замена слов длиннее 2 символов
+                                if (change.Line.Any(@char => char.IsLetter(@char)))
+                                {
+                                    change.Split();
+                                    change.UpperCase();
+                                    change.Join();
+                                    change.Tabulation();
+                                }
+                                //
+
+                                bufer.AppendLine(change.Line);
+
+                                // замена private на public
+                                bufer.Replace("public", "private");
+                                //
+
+                                newBufer.Write(bufer);
+
+                                bufer.Clear();
+
+                                change.PrevLine = change.Line;
+                            }
+                        }
+                    }
+
+                    using (StreamWriter writer = new StreamWriter(path, false))
+                    {
+                        writer.Write(newBufer);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
