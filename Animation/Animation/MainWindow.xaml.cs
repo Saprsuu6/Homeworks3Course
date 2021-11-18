3b1ec3c82f1f011fe9aab4b77f0940f8fc11db1a
Saprsuu6 <coffeei.2002@gmail.com>
2021-11-18 11:19:09 +0300

    HW by 11/18/2021

---
+++ Animation/Animation/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Animation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation sizeAnimation = new DoubleAnimation();
+            sizeAnimation.From = Text.FontSize;
+            sizeAnimation.To = 30;
+            sizeAnimation.Duration = TimeSpan.FromSeconds(1);
+            sizeAnimation.DecelerationRatio = 1;
+
+            DoubleAnimation widthAnimation = new DoubleAnimation();
+            widthAnimation.From = Button1.Width;
+            widthAnimation.To = Width - 25;
+            widthAnimation.Duration = TimeSpan.FromSeconds(1);
+            widthAnimation.Completed += AnimationCompleted;
+            widthAnimation.DecelerationRatio = 1;
+
+            Text.BeginAnimation(Button.FontSizeProperty, sizeAnimation);
+            Button1.BeginAnimation(Button.WidthProperty, widthAnimation);
+        }
+
+        private void AnimationCompleted(object sender, EventArgs e)
+        {
+            DoubleAnimation sizeAnimation = new DoubleAnimation();
+            sizeAnimation.From = Text.FontSize;
+            sizeAnimation.To = 20;
+            sizeAnimation.Duration = TimeSpan.FromSeconds(5);
+            sizeAnimation.DecelerationRatio = 1;
+
+            DoubleAnimation widthAnimation = new DoubleAnimation();
+            widthAnimation.From = Button1.Width;
+            widthAnimation.To = 400;
+            widthAnimation.Duration = TimeSpan.FromSeconds(5);
+            widthAnimation.DecelerationRatio = 1;
+
+            Text.BeginAnimation(Button.FontSizeProperty, sizeAnimation);
+            Button1.BeginAnimation(Button.WidthProperty, widthAnimation);
+        }
+
+        private void Button1_MouseLeave(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation sizeAnimation = new DoubleAnimation();
+            sizeAnimation.From = Text.FontSize;
+            sizeAnimation.To = 16;
+            sizeAnimation.Duration = TimeSpan.FromSeconds(1);
+            sizeAnimation.DecelerationRatio = 1;
+
+            DoubleAnimation widthAnimation = new DoubleAnimation();
+            widthAnimation.From = Button1.Width;
+            widthAnimation.To = 200;
+            widthAnimation.Duration = TimeSpan.FromSeconds(1);
+            widthAnimation.DecelerationRatio = 1;
+
+            Text.BeginAnimation(Button.FontSizeProperty, sizeAnimation);
+            Button1.BeginAnimation(Button.WidthProperty, widthAnimation);
+        }
+    }
+}
