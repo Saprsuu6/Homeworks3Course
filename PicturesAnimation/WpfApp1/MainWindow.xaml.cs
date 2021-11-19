3080c994d83bf7294112ddc8aa1854e30b02043c
Saprsuu6 <coffeei.2002@gmail.com>
2021-11-19 18:08:51 +0300

    HW by 11/19/2021

---
+++ PicturesAnimation/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AnimationCompleted(object sender, EventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+        }
+
+        private void storyboard_CurrentTimeInvalidated(object sender, EventArgs e)
+        {
+            // Sender is the clock that was created for this storyboard.
+            Clock storyboardClock = (Clock)sender;
+
+            if (storyboardClock.CurrentProgress == null)
+            {
+                lblTime.Text = "[[ stopped ]]";
+                progressBar.Value = 0;
+            }
+            else
+            {
+                lblTime.Text = storyboardClock.CurrentTime.ToString();
+                progressBar.Value = (double)storyboardClock.CurrentProgress;
+            }
+        }
+
+        private void sldSpeed_ValueChanged(object sender, RoutedEventArgs e)
+        {
+            fadeStoryboard.SetSpeedRatio(this, sldSpeed.Value);
+        }
+    }
+}
